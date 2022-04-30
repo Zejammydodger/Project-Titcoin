a96a9a6434abec91edbcd3c26fa1f4fbd90c3769
ethanparab <59212466+ethanparab@users.noreply.github.com>
2022-04-29 21:17:26 -0500

    Well I fixed one error. Now it's mad at me and giving me the silent treatment (silent sql error later on)

--- extensions/titcoin.py
+++ extensions/titcoin.py
@@ -3,6 +3,7 @@ import discord , asyncio , math , random , datetime
 from discord.ext import commands , tasks
 from titcoinHelpers import NoVoice , Denied , HasCompany
 from sqlHelper import Profile , Company , Share , load , save , initDataBase , blankHistory
+import datetime
 
 #the actual titcoin functionality
 
@@ -11,7 +12,7 @@ cooldownTime = 60
 
 connection = initDataBase()
 
-profiles : dict[str : dict | list] = load(connection) 
+profiles : dict[str : dict | list] = load(connection)
     #{"profiles" : { discordID : Profile } , "companies" : [Company]}
 #print(profiles)
 percs = []
@@ -178,7 +179,7 @@ class StartCompany(Perc):
         await ctx.send(embed = emb)
         
         while True:
-            message : discord.Message = await self.bot.wait_for("message" , check = checkFactory(ctx.author , lambda x : x.isdigit()))
+            message : discord.Message = await self.bot.wait_for("message" , check = checkFactory(ctx.author , lambda x : str(x).isdigit()))
             if message is None:
                 await ctx.send("you failed to respond in time, try again")
             else:
@@ -191,7 +192,10 @@ class StartCompany(Perc):
         Comp = Company(P , blankHistory() , [] , name) # creates company and adds it to profile
         Comp.createShare(P , self.currentPrice + extra) # creates share, recalculates and adds to profile
         profiles["companies"].append(Comp) #add comp to database
-        
+
+        print(f"{Comp.name}, established in {datetime.datetime.year}")
+        emb = discord.Embed(title=f"{Comp.name}, established in {datetime.datetime.year}", description=f"Founded by {Comp.owner}.\n{Comp.shares}")
+        await ctx.send(embed=emb)
     
         
 class MuteFriendPerc(Perc):
@@ -330,6 +334,8 @@ class TitCoin(commands.Cog):
         print("flag")
          # tiddleton
         self.tiddleton = self.bot.get_guild(693537199500689439)
+        if self.tiddleton is None:                  # it took me an embarrassing amount of time to realize the bot looks for tiddleton specifically. Anyways this is my test server
+            self.tiddleton = self.bot.get_guild(969731141890363402)
         assert self.tiddleton is not None , "Bot not in tiddleton"
         for mem in self.tiddleton.members:
             if mem.id not in profiles["profiles"].keys():
