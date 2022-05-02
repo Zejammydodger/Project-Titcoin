a558511ffdc7bf4eca7c7c199834aeb027f7f171
ethanparab <59212466+ethanparab@users.noreply.github.com>
2022-05-02 16:33:49 -0500

    made a few changes to the configuration to make it a little more orm friendly

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -4,39 +4,47 @@ initialize the database if it isnt already initialized
 CREATE DATABASE IF NOT EXISTS TitCoin;
 USE TitCoin;
 
+/* profiles of all members, along with their balances */
 CREATE TABLE IF NOT EXISTS Profiles(
     discordID BIGINT NOT NULL,
+    balance DOUBLE NOT NULL DEFAULT 0,
     PRIMARY KEY(discordID)
 );
 
+/* log of balance changes */
+CREATE TABLE IF NOT EXISTS BalanceHistory(
+    PID BIGINT,
+    FOREIGN KEY(PID) REFERENCES Profiles(discordID),
+    balance FLOAT NOT NULL,
+    timestamp DATETIME NOT NULL
+);
+
+/* all companies, along with their associated owners */
 CREATE TABLE IF NOT EXISTS Companies(
     CID INT AUTO_INCREMENT,
     PID BIGINT,
     FOREIGN KEY(PID) REFERENCES Profiles(discordID), 
-    CompanyName TEXT NOT NULL,
+    companyName TEXT NOT NULL,
+    worth DOUBLE NOT NULL,
+    shares INT,
     PRIMARY KEY(CID)
 );
 
+/* history of worth of companies */
 CREATE TABLE IF NOT EXISTS WorthHistory(
     CID INT,
     FOREIGN KEY(CID) REFERENCES Companies(CID),
-    created DATETIME NOT NULL,
-    worth FLOAT NOT NULL
+    worth FLOAT NOT NULL,
+    timestamp DATETIME NOT NULL
 );
 
+/* record of share ownership */
 CREATE TABLE IF NOT EXISTS Shares(
     ShareID INT AUTO_INCREMENT NOT NULL,
     PID BIGINT,
     CID INT,
     FOREIGN KEY(PID) REFERENCES Profiles(discordID),
     FOREIGN KEY(CID) REFERENCES Companies(CID),
-    percent FLOAT NOT NULL,
+    numShares BIGINT NOT NULL,
     PRIMARY KEY(ShareID)
-);
-
-CREATE TABLE IF NOT EXISTS BalanceHistory(
-    PID BIGINT,
-    FOREIGN KEY(PID) REFERENCES Profiles(discordID),
-    balance FLOAT NOT NULL,
-    created DATETIME NOT NULL
 );
