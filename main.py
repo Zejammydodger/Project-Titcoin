27388fc6eec640c1720af7e5feb003c3816f3f99
Zejammydodger <james.cleaver1@outlook.com>
2022-05-03 20:54:45 +0100

    fucky slash command bullshit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import discord , os
 from discord.ext import commands
 from cogwatch import Watcher
+from discord_ui import UI
+from discord_ui import SlashInteraction , UI , Button , LinkButton
+from discord_ui.cogs import slash_command, subslash_command, listening_component
 
 # this is the main file of the bot, all globals and shit happen here
 
@@ -9,10 +12,16 @@ modules = ["titcoin"]   # add extension names here
 extFolder = "extensions"
 
 bot = commands.Bot("$", intents=discord.Intents.all())
+ui = UI(bot)
+
 
 for ext in modules:
     bot.load_extension(f"{extFolder}.{ext}")
 
+
+@ui.slash.command(name="slashTest" , guild_ids=[693537199500689439])
+async def command(ctx):
+    await ctx.respond("yep")
 
 @bot.event
 async def on_ready():
