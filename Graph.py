0eb8ab8faa51bc1ede821f903bda145c47a9e651
Zejammydodger <james.cleaver1@outlook.com>
2022-05-04 09:07:56 +0100

    text graph works

--- Graph.py
+++ Graph.py
@@ -1,4 +1,8 @@
 #like table.py but for actual properly orientated graphs
+import random
+from re import A
+
+from click import Abort
 from Table import typesafeCharNum
 from math import floor
 
@@ -11,11 +15,11 @@ class Graph:
         self.yAxis = yAxisName
         self.char = pointChar
         self.points : list[tuple] = []
-        self.maxVal_x = 1000000000
-        self.maxVal_y = 1000000000
-        self.minVal_x = -1000000000
-        self.minVal_y = -1000000000
-        self.__graph = [[False for w in range(width)] for h in range(height)]
+        self.maxVal_x = -1000000000
+        self.maxVal_y = -1000000000
+        self.minVal_x = 1000000000
+        self.minVal_y = 1000000000
+        self.__graph = [[" " for w in range(width)] for h in range(height)]
         
     def addPointTuple(self , point : tuple):
         
@@ -32,16 +36,55 @@ class Graph:
         self.addPointTuple((x , y))
         
     def __scaleToX(self , x) -> int:
-        return floor((x / (self.maxVal_x - self.minVal_x)) * self.width) 
+        return floor((x / (self.maxVal_x - self.minVal_x)) * self.width) - 1 
         
     def __scaleToY(self , y) -> int:
-        return floor((y / (self.maxVal_y - self.minVal_y)) * self.height)
+        return floor((y / (self.maxVal_y - self.minVal_y)) * self.height) - 1
         
     def plotPoints(self):
         #go through th points, scales to maxVal , minVal
         for x , y in self.points:
-            self.__graph[self.__scaleToY(y)][self.__scaleToX(x)] = True
+            sy = self.__scaleToY(y)
+            sx = self.__scaleToX(x)
+            self.__graph[sy][sx] = self.char
         
     def __str__(self):
         #this will take all of the data and spit out a nicely formatted graph
-        pass
+        self.plotPoints()
+        yPadding = typesafeCharNum(self.maxVal_y)
+        yAxisLen = typesafeCharNum(self.yAxis)
+        rows = [] # actual data (plus y axis but it doesnt start like that shhhhhh)
+        for i , row in enumerate(self.__graph):
+            line = "".join(row)
+            if i == 0:
+                axis = f"{str(self.maxVal_y)}|" #this places the maximum value at the top
+            else:    
+                axis = f"{' '*yPadding}|"  #padding for comment above
+                
+            rows.append(axis+line+"\n")
+        
+        ypad = floor((self.height-yAxisLen)/2)
+        xpad = floor((self.width-typesafeCharNum(self.xAxis))/2)
+        yAxisName = f"{' '*ypad}{self.yAxis}{' '*ypad}" # this centres the yaxis name
+        xAxisName = f"{' '*xpad}{self.xAxis}{' '*xpad}" 
+        for i , c in enumerate(yAxisName):
+            r = rows[i]
+            rows[i] = c + r # this adds the yaxis name down the side
+            
+        a = f"{' '*yPadding}"
+        b = f"{'-'*self.width}"
+        c = f"{' ' * (1+yPadding+self.width-typesafeCharNum(self.maxVal_x))}"
+        d = f"{str(self.maxVal_x)}"
+        e = f"{' '*(yPadding+1)}"
+        f = f"{xAxisName}"
+
+        footer = f"{a}*{b}\n{c}{d}\n{e}{f}" # adds the bottom axis 
+        return "".join(rows) + footer
+    
+    
+    
+if __name__ == "__main__":
+    g = Graph("random" , "random")
+    for i in range(100):
+        g.addPoint(random.randint(0 , 1000) , random.randint(0 , 1000))
+    print(g)
