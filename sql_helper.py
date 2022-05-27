46b4ac8c94c583f59263e8bf506986b67406e625
ethanparab <59212466+ethanparab@users.noreply.github.com>
2022-05-26 22:06:51 -0500

    added like a bunch of comments and changed some access

--- sql_helper.py
+++ sql_helper.py
@@ -13,6 +13,11 @@ mapper_registry = orm.registry()
 Base = mapper_registry.generate_base()
 
 
+# helper functions
+def get_session():
+    return orm.sessionmaker(bind=engine, autocommit=True).begin()
+
+
 def format_time(_datetime: datetime.datetime):
     return _datetime.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -21,12 +26,15 @@ def get_time():
     return format_time(datetime.datetime.utcfromtimestamp(time.time()))
 
 
+# mapping of user profiles
 class Profile(Base):
     __tablename__ = "profiles"
 
+    # columns
     id = sq.Column("id", sq.BigInteger, primary_key=True, nullable=False)
-    balance = sq.Column("balance", sq.Numeric(14, 2))
+    _balance = sq.Column("balance", sq.Numeric(14, 2))
 
+    # relationships
     companies = orm.relationship("Company", back_populates="owner")
     history = orm.relationship("BalanceSlice", back_populates="profile")
     share_entries = orm.relationship("ShareEntry", back_populates="profile")
@@ -34,27 +42,33 @@ class Profile(Base):
     def __init__(self, id: int, balance: Decimal):
         super().__init__()
         self.id = id
-        self.balance = balance
+        self._balance = balance
 
-    def change_balance(self, amount, time: datetime.datetime = None, tag: str = None):
-        self.balance += amount
+    # changes the balance of a user (it supports negative numbers btw)
+    def change_balance(self, amount: Decimal, time: datetime.datetime = None, tag: str = None):
+        # sets the balance
+        self._balance += amount
 
+        # adds a log into the history
         session: orm.Session = orm.Session.object_session(self)
-        session.add(BalanceSlice(self, self.balance, time, tag))
+        session.add(BalanceSlice(self, self._balance, time, tag))
         session.flush()
 
     def __repr__(self):
-        return f"Profile(id={self.id!r}, balance={self.balance!r}, companies={', '.join(i.name for i in self.companies)})"
+        return f"Profile(id={self.id!r}, balance={self._balance!r}, companies={', '.join(i.name for i in self.companies)})"
 
 
+# mapping of companies
 class Company(Base):
     __tablename__ = "companies"
 
+    # columns
     id = sq.Column("id", sq.Integer, primary_key=True, nullable=False, autoincrement=True)
     _profile_id = sq.Column("profile_id", sq.BigInteger, sq.ForeignKey("profiles.id"))
     name = sq.Column("name", sq.Text)
     worth = sq.Column("worth", sq.Numeric(14, 2))
 
+    # relationships
     owner = orm.relationship("Profile", uselist=False, back_populates="companies")
     history = orm.relationship("WorthSlice", uselist=False, back_populates="company")
     share_entries = orm.relationship("ShareEntry", back_populates="company")
@@ -69,15 +83,18 @@ class Company(Base):
         return f"Company(id={self.id!r}, name={self.name!r}, worth={self.worth!r})"
 
 
+# mapping of profile balance history entry
 class BalanceSlice(Base):
     __tablename__ = "balancehistory"
 
+    # columns
     id = sq.Column("id", sq.Integer, primary_key=True, nullable=False, autoincrement=True)
     _profile_id = sq.Column("profile_id", sq.BigInteger, sq.ForeignKey("profiles.id"), nullable=False)
     balance = sq.Column("balance", sq.Numeric(14, 2), nullable=False)
     time = sq.Column("time", sq.DateTime, nullable=False)
     tag = sq.Column("tag", sq.Text)
 
+    # relationships
     profile = orm.relationship("Profile", uselist=False, back_populates="history")
 
     def __init__(self, profile: Profile, balance: Decimal, time: datetime.datetime = None, tag: str = None):
@@ -91,15 +108,18 @@ class BalanceSlice(Base):
         return f"BalanceSlice(id={self.id!r}, profile_id={self._profile_id!r}, balance={self.balance!r}, time={self.time!r}, tag={self.tag!r})"
 
 
+# mapping of company worth history entry
 class WorthSlice(Base):
     __tablename__ = "worthhistory"
 
+    # columns
     id = sq.Column("id", sq.Integer, primary_key=True, nullable=False, autoincrement=True)
     _company_id = sq.Column("company_id", sq.Integer, sq.ForeignKey("companies.id"), nullable=False)
     worth = sq.Column("worth", sq.Numeric(14, 2), nullable=False)
     time = sq.Column("time", sq.DateTime, nullable=False)
     tag = sq.Column("tag", sq.Text)
 
+    # relationships
     company = orm.relationship("Company", uselist=False, back_populates="history")
 
     def __init__(self, company: Company, worth: Decimal, time: datetime.datetime = None, tag: str = None):
@@ -113,14 +133,17 @@ class WorthSlice(Base):
         return f"WorthSlice(id={self.id!r}, company_id={self._company_id!r}, worth={self.worth!r}, time={self.time!r}, tag={self.tag!r})"
 
 
+# mapping of share ownership entry
 class ShareEntry(Base):
     __tablename__ = "shares"
 
+    # columns
     id = sq.Column("id", sq.Integer, primary_key=True, nullable=False, autoincrement=True)
     _profile_id = sq.Column("profile_id", sq.BigInteger, sq.ForeignKey("profiles.id"), nullable=False)
     _company_id = sq.Column("company_id", sq.Integer, sq.ForeignKey("companies.id"), nullable=False)
     num_shares = sq.Column("num_shares", sq.BigInteger, nullable=False)
 
+    # relationships
     profile = orm.relationship("Profile", uselist=False, back_populates="share_entries")
     company = orm.relationship("Company", uselist=False, back_populates="share_entries")
 
@@ -134,11 +157,8 @@ class ShareEntry(Base):
         return f"ShareEntry(id={self.id!r}, profile_id={self._profile_id!r}, company_id={self._company_id!r}, num_shares={self.num_shares!r})"
 
 
+# builds all the mapped classes
 mapper_registry.metadata.create_all(engine)
-
-
-def get_session():
-    return orm.sessionmaker(bind=engine, autocommit=True).begin()
 
 
 if __name__ == "__main__":
